67707c809bdf1d198c92000734797e02666fa37b
Utkarsh Naman <114791877+utkarsh-naman@users.noreply.github.com>
2025-04-09 21:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def index():
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
